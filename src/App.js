79f0e503849c69ec378b4a7b585b91a4b8a8d894
Sanjesh kumar <130355594+Sanjeshkumar@users.noreply.github.com>
2023-06-02 12:57:43 +0530

    this form contains login and signup connected with firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { app } from './Firebase';
+import { useState } from 'react';
+import { Home } from './Home';
+import { Routes,Route } from 'react-router-dom';
+
 
 function App() {
+  const auth = getAuth(app); //exported form the fire base
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signUp =()=>{
+   
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+        alert("successfully created");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        //const errorMessage = error.message;
+        alert(errorCode);
+        // ..
+      });
+  
+  }
+
+  const signIn=()=>{
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    alert("succcessfully login")
+   
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    //const errorMessage = error.message;
+    alert(errorCode);
+  });
+  }
+  
+  const inputStyles = {
+    padding: '10px',
+    width: '250px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    marginBottom: '10px',
+  };
+
+  const buttonStyles = {
+    padding: '10px 20px',
+    backgroundColor: '#4caf50',
+    color: 'white',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  };
+
+  const mainStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  };
+
+  const appStyles = {
+    backgroundColor: '#f5f5f5',
+    borderRadius: '5px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+    padding: '20px',
+    textAlign: 'center',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div style={mainStyles}>
+        <div style={appStyles}>
+          <input
+            type="email"
+            placeholder="Your Email ID"
+            style={inputStyles}
+            onChange={(e) => setEmail(e.target.value)
+            }
+          /><br /><br />
+          <input
+            type="password"
+            placeholder="Enter password"
+            style={inputStyles}
+            onChange={(e) => setPassword(e.target.value)}
+          /><br /><br />
+          <button style={buttonStyles} onClick={signUp}>Create Account</button><br />
+          <button style={{buttonStyles,marginTop:'10px'}}  onClick={signIn}>Sign In</button>
+        </div>
+        <Routes>
+          <Route exact path='/' element={<signIn/>}/>
+        </Routes>
+      </div>
+    </center>
   );
-}
+};
 
 export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_b0FV5Nms7O6X7XehuJrPJDlyYXitQkU",
+  authDomain: "login-sign-up-84614.firebaseapp.com",
+  projectId: "login-sign-up-84614",
+  storageBucket: "login-sign-up-84614.appspot.com",
+  messagingSenderId: "115986862099",
+  appId: "1:115986862099:web:b4e70557a6ca29918e6fe6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
